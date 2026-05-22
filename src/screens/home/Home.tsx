e91ce55750f330550b823ecaf370e50dfbce08e8
daniel34workspace <daniel05muniz@gmail.com>
2026-05-22 20:20:49 -0300

    Commit inicial

---
+++ src/screens/home/Home.tsx
@@ -0,0 +1,144 @@
+// COMPONENTES NATIVOS
+import React, { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { View, Image, Text, TouchableOpacity, TextInput, Alert } from 'react-native';
+import { styles } from './HomeStyles';
+import kuromiLogo from './../../../assets/Images/kuromi-logo.png'
+import kuromiInvite from './../../../assets/Images/kuromi-invite.png'
+import kuromiInvite2 from './../../../assets/Images/kuromi-invite2.png'
+
+
+export default function Home() {
+
+    const [form, setForm] = useState(true);
+    const [textName , setTextName] = useState<string>('');
+    const [textTel , setTel] = useState<string>('');
+    const [textEnder , setEnder] = useState<string>('');
+    const [textMens , setMens] = useState<string>('');
+
+    const hasText = (textName.length > 0 &&
+        textTel.length > 0 &&
+        textEnder.length > 0 &&
+        textMens.length > 0 
+    )
+
+    function mostraForm() {
+        setForm(!form)
+        console.log(form)
+    }
+
+    function enviaForm() {
+        if (hasText) {
+            Alert.alert(
+                'Completo',
+                'Seu convite foi envido'
+            );
+            mostraForm()
+        } else {
+            Alert.alert(
+                'Incompleto',
+                'Preencha todos os dados antes de enviar o convite'
+            );
+
+        }
+    }
+
+
+    return (
+        <View style={styles.container}>
+
+            {/**  KUROMI LOGO */}
+            <View style={[styles.view2, { display: form ? 'flex' : 'none' }]}>
+                <Image
+                    style={styles.kuromiLogo}
+                    source={kuromiLogo}>
+                </Image>
+            </View>
+
+            {/**  KUROMI BUTTON */}
+            <View style={[styles.butao, { display: form ? 'flex' : 'none' }]}>
+                <TouchableOpacity
+                    style={styles.viewButao}
+                    onPress={mostraForm}
+                >
+                    <Text
+                        style={styles.texto}
+                    >🎀 KUROMI PARTY 🎀</Text>
+                </TouchableOpacity>
+            </View>
+
+            {/** KUROMI FORMULÁRIO */}
+            <View style={[styles.form, { display: form ? 'none' : 'flex' }]}>
+
+                <View style={styles.kuromiInviteSpace}>
+                    <Image
+                        style={styles.kuromiInvite}
+                        source={kuromiInvite2}>
+                    </Image>
+                </View>
+
+                <View style={styles.ViewLabel}>
+                    <Text style={styles.label}>Nome: </Text>
+                </View>
+                <View style={styles.containerTextInput}>
+                    <TextInput
+                        placeholder='Nome'
+                        onChangeText={setTextName}
+                        multiline={true}
+                        style={styles.inputer}
+                    ></TextInput>
+                </View>
+
+                <View style={styles.ViewLabel}>
+                    <Text style={styles.label}>Telefone: </Text>
+                </View>
+                <View style={styles.containerTextInput}>
+                    <TextInput
+                        placeholder='Telefone'
+                        onChangeText={setTel}
+                        multiline={true}
+                        style={styles.inputer}
+                    ></TextInput>
+                </View>
+
+                <View style={styles.ViewLabel}>
+                    <Text style={styles.label}>Endereço: </Text>
+                </View>
+                <View style={styles.containerTextInput}>
+                    <TextInput
+                        placeholder='Endereço'
+                        onChangeText={setEnder}
+                        multiline={true}
+                        style={styles.inputer}
+                    ></TextInput>
+                </View>
+
+                <View style={styles.ViewLabel}>
+                    <Text style={styles.label}>Mensagem especial: </Text>
+                </View>
+                <View style={styles.containerTextInput}>
+                    <TextInput
+                        placeholder='Mensagem'
+                        onChangeText={setMens}
+                        multiline={true}
+                        style={[styles.inputer, styles.inputerMens]}
+                    ></TextInput>
+
+                </View>
+
+                <View style={styles.butaoEnviar}>
+                    <TouchableOpacity
+                        style={styles.viewButaoEnviar}
+                        onPress={enviaForm}
+                    >
+                        <Text
+                            style={styles.texto}
+                        >🎀 ENVIAR 🎀</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+
+            <StatusBar style={'dark'} />
+        </View>
+    );
+}
---
+++ src/screens/home/HomeStyles.tsx
@@ -0,0 +1,156 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#d6c3e2',
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%'
+  },
+
+  secondContainer: {
+    width: '100%'
+  },
+
+  butao: {
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  viewButao: {
+    flex: 0,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#a95deb',
+    width: '85%',
+    padding: 20,
+    borderColor: '#e533a1',
+    borderWidth: 5,
+    borderRadius: 20,
+    elevation: 5,
+  },
+
+  texto: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+
+  view2: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 400,
+    width: 400,
+    borderWidth: 5,
+    borderColor: '#e533a1',
+    backgroundColor: '#fff',
+    borderRadius: 250,
+    marginBottom: 50,
+    elevation: 5
+  },
+
+  kuromiLogo: {
+    height: 280,
+    width: 280,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  containerTextInput: {
+    flex: 0,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '85%',
+  },
+
+  inputer: {
+    marginTop: 10,
+    backgroundColor: '#ffffff',
+    width: '100%',
+    borderWidth: 3,
+    borderColor: '#e533a1',
+    borderRadius: 20,
+    paddingHorizontal: 20,
+    height: 60,
+    elevation: 5
+  },
+
+  inputerMens: {
+    height: 180,
+    alignSelf: 'flex-start'
+  },
+
+  form: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+  },
+
+  kuromiInviteSpace: {
+    backgroundColor: '#e533a1',
+    width: '50%',
+    height: 125,
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    borderRadius: 20,
+    elevation: 5
+  },
+
+  kuromiInvite: {
+    height: 150,
+    width: 150,
+    
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    elevation: 5
+  },
+
+  ViewLabel: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '80%',
+
+  },
+
+  label: {
+    fontWeight: '800',
+    fontFamily: 'georgia',
+    alignSelf: 'flex-start',
+    color: '#bb0989',
+    marginTop: 20
+  },
+
+  butaoEnviar: {
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  viewButaoEnviar: {
+    marginTop: 20,
+    flex: 0,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#a95deb',
+    width: '85%',
+    padding: 12,
+    borderColor: '#e533a1',
+    borderWidth: 5,
+    borderRadius: 20,
+    elevation: 5,
+  },
+
+});
